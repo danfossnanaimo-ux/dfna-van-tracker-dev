f251f44129b95318e26c3a5b88cd4f03a70b0a1c
danfossnanaimo-ux <dan.foss.nanaimo@gmail.com>
2026-05-18 11:11:25 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,13 +10,13 @@ self.addEventListener("fetch", event => {
   const url = new URL(event.request.url);
 
   // BYPASS SW FOR ALL MAP TILES
-  if (
-    url.hostname.includes("tile") ||
-    url.hostname.includes("openstreetmap") ||
-    url.pathname.match(/\.(png|jpg|jpeg)$/)
-  ) {
-    return; // Let browser fetch normally
-  }
+  //if (
+  //  url.hostname.includes("tile") ||
+  //  url.hostname.includes("openstreetmap") ||
+  //  url.pathname.match(/\.(png|jpg|jpeg)$/)
+  //) {
+  //  return; // Let browser fetch normally
+  //}
 
   // Everything else: network only
   event.respondWith(fetch(event.request));
