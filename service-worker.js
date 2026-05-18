033cae209477890278eaf7ebab9c2f0270fbdb71
danfossnanaimo-ux <dan.foss.nanaimo@gmail.com>
2026-05-18 08:31:57 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,11 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  caches.keys().then(keys => {
-    return Promise.all(keys.map(k => caches.delete(k)));
-  }).then(() => {
-    self.clients.claim();
-  });
+self.addEventListener("activate", event => {
+  clients.claim();
 });
 
-// Do NOT cache anything
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
